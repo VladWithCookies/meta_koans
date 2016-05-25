a3acf21cc9478dce69e2bad3af9e8267f952e2c4
Vlad V <summervibeswith9pm@gmail.com>
2016-05-25 17:00:31 +0300

    Update knowledge.rb

--- knowledge.rb
+++ knowledge.rb
@@ -1,23 +1,17 @@
 class Module
-	def attribute(a, &block)	
-		a, default = a.to_a.first if a.is_a? Hash
-		var = :"@#{a}"
-
-		define_method(:"#{a}?") { send(a) }
-
-		define_method :"#{a}=" do |value|
-			instance_variable_set(var, value)
-		end
-
-		define_method :"#{a}" do 
-			if instance_variables.include? var
-				instance_variable_get var
-			elsif block
-				instance_eval(&block)
-			else
-				instance_variable_set(var, default)
-			end
-		end
-
-	end
+  def attribute(a, &block)	
+    a, default = a.to_a.first if a.is_a? Hash
+    var = :"@#{a}"
+    define_method(:"#{a}?") { send(a) }
+    define_method :"#{a}=" { |value| instance_variable_set(var, value) }
+    define_method :"#{a}" do 
+      if instance_variables.include? var
+        instance_variable_get var
+      elsif block			
+        instance_eval(&block)
+      else
+	instance_variable_set(var, default)
+      end
+    end
+  end
 end
